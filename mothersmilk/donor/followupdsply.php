aaf3aead72a81006b67265681c384a6ff7bf1dce
thanhau <thanhau123@yahoo.com>
2013-03-20 11:12:49 -0700

    update

---
+++ mothersmilk/donor/followupdsply.php
@@ -0,0 +1,28 @@
+<?php
+	
+	$row['donorpacketbymail'],
+			'donorpacketbyemail'   => $_POST['donorpacketbyemail'],
+			'donorpacketbyfax'   => $_POST['donorpacketbyfax'],
+			'datesentpacket'   => $_POST['datesentpacket'],
+			'staffinitdatesentpacket' => $_POST['staffinitdatesentpacket'],
+			'datereceivedpacket'  => $_POST['datereceivedpacket'],
+			'staffinitdatereceivedpacket'      => $_POST['staffinitdatereceivedpacket'],
+
+			'pedifaxneeded'   => $_POST['pedifaxneeded'],
+			'datesentpedi'   => $_POST['datesentpedi'],
+			'staffinitdatesentpedi' => $_POST['staffinitdatesentpedi'],
+			'datereceivedpedi'  => $_POST['datereceivedpedi'],
+			'staffinitdatereceivedpedi'      => $_POST['staffinitdatereceivedpedi'],
+			
+			'obfaxneeded'   => $_POST['obfaxneeded'],
+			'datesentob'   => $_POST['datesentob'],
+			'staffinitdatesentob' => $_POST['staffinitdatesentob'],
+			'datereceivedob'  => $_POST['datereceivedob'],
+			'staffinitdatereceivedob'      => $_POST['staffinitdatereceivedob'],
+			
+			'other'   => $_POST['other'],
+			'datesentother'   => $_POST['datesentother'],
+			'staffinitdatesentother' => $_POST['staffinitdatesentother'],
+			'datereceivedother'  => $_POST['datereceivedother'],
+			'staffinitdatereceivedother'      => $_POST['staffinitdatereceivedother'],
+?>
